97cb01e119e4045901fc366125a610ab086edca5
Dante Vannini <dante.vannini@trends.nz>
2026-05-06 13:03:19 +1200

    added creds

--- config.py
+++
@@ -1,7 +0,0 @@
-# ---- Akahu ----
-AKAHU_USER_TOKEN = "YOUR_AKAHU_USER_TOKEN"
-
-# ---- Google Sheets ----
-GOOGLE_CREDS_FILE = "google_creds.json"
-SPREADSHEET_NAME = "Budget Tracker"
-WORKSHEET_NAME = "Sheet1"
